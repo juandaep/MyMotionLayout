9e8ced3fa820acfec03dd7d912a2b94942fedcb7
Juanda <juandaesaputra@gmail.com>
2020-10-13 07:18:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mymotionlayout/SwipeRightCardModel.kt
@@ -0,0 +1,7 @@
+package com.example.mymotionlayout
+
+import androidx.annotation.ColorInt
+
+data class SwipeRightCardModel(
+    @ColorInt val backgroundColor: Int
+)
---
+++ app/src/main/java/com/example/mymotionlayout/SwipeRightModel.kt
@@ -0,0 +1,6 @@
+package com.example.mymotionlayout
+
+data class SwipeRightModel(
+    val top: SwipeRightCardModel,
+    val bottom: SwipeRightCardModel
+)
